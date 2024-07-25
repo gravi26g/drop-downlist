3aab88baa1f1485b6f2efe71136c7c96eac536a5
gravi26g <ravigokanas@gmail.com>
2024-07-25 12:55:51 +0530

    updatenumber

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
     {id:2, name:"2"},
     {id:3, name:"3"},
     {id:4, name:"4"},
-    
+    {id:5,name:"5"}
     
   ]
   constructor(private fb: FormBuilder) {}
